cfec39775f8dd5b99ef9793d7d80d76d02799379
Amil Masic <amil.masic@gmail.com>
2018-10-06 10:56:19 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,8 +15,11 @@ class Patient
   def new_appointment
     appointment = Appointment.new(date, doctor_name, self)
     doctor = Doctor.new(doctor_name)
-
-
+    @appointments << appointment
+    appointment
+  end
+  
+    
 
 
 end
