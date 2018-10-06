30536b18513e03d70c12c32dc843ce791bcaeb6a
Amil Masic <amil.masic@gmail.com>
2018-10-06 10:18:55 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+require "pry"
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(song_name, genre_name)
+    # binding.pry
+    song = Song.new(song_name, self, genre_name)
+    genre = Genre.new(genre_name)
+    @songs << song
+    song
+  end
+
+  def songs
+    @songs
+  end
+
+end
